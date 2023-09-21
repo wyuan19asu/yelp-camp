86493ed4dc608679b2e4bfa92df59f6ec6d2fdfc
wyuan19asu <wyuan19@asu.edu>
2023-09-21 03:19:18 -0700

    started basic error handling for creating new camp

--- app.js
+++ app.js
@@ -48,10 +48,14 @@ app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
 
-app.post("/campgrounds/", async (req, res) => {
-  const campground = new Campground(req.body.campground);
-  await campground.save();
-  res.redirect(`/campgrounds/${campground.id}`);
+app.post("/campgrounds/", async (req, res, next) => {
+  try {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground.id}`);
+  } catch (e) {
+    next(e);
+  }
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
@@ -83,6 +87,10 @@ app.delete("/campgrounds/:id", async (req, res) => {
   const { id } = req.params;
   const deleted = await Campground.findByIdAndDelete(id);
   res.redirect("/campgrounds/");
+});
+
+app.use((err, req, res, next) => {
+  res.send("Oh boy! Something went wrong");
 });
 
 app.listen(3000, () => {

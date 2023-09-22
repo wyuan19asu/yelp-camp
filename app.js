987c696a0ba6fbe2550358bf4f2e75f025856131
wyuan19asu <wyuan19@asu.edu>
2023-09-21 19:05:59 -0700

    added error validation with error class

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground");
 const methodOverride = require("method-override");
+const catchAsync = require("./utils/catchAsync");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/yelp-camp")
@@ -39,55 +40,72 @@ app.get("/", (req, res) => {
 //   res.send(camp);
 // });
 
-app.get("/campgrounds", async (req, res) => {
-  const campgrounds = await Campground.find({});
-  res.render("campgrounds/index", { campgrounds });
-});
+app.get(
+  "/campgrounds",
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
 
-app.get("/campgrounds/new", (req, res) => {
-  res.render("campgrounds/new");
-});
+app.get(
+  "/campgrounds/new",
+  catchAsync((req, res) => {
+    res.render("campgrounds/new");
+  })
+);
 
-app.post("/campgrounds/", async (req, res, next) => {
-  try {
+app.post(
+  "/campgrounds/",
+  catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground.id}`);
-  } catch (e) {
-    next(e);
-  }
-});
+  })
+);
 
-app.get("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id);
-  res.render("campgrounds/show", { campground });
-});
+app.get(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/show", { campground });
+  })
+);
 
-app.get("/campgrounds/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id);
-  res.render("campgrounds/edit", { campground });
-});
+app.get(
+  "/campgrounds/:id/edit",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/edit", { campground });
+  })
+);
 
-app.put("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(
-    id,
-    { ...req.body.campground },
-    {
-      runValidator: true,
-      new: true,
-    }
-  );
-  res.redirect(`/campgrounds/${campground._id}`);
-});
+app.put(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(
+      id,
+      { ...req.body.campground },
+      {
+        runValidator: true,
+        new: true,
+      }
+    );
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
 
-app.delete("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  const deleted = await Campground.findByIdAndDelete(id);
-  res.redirect("/campgrounds/");
-});
+app.delete(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const deleted = await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds/");
+  })
+);
 
 app.use((err, req, res, next) => {
   res.send("Oh boy! Something went wrong");

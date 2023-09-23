d0a7c4e603237c131eae34049b6f6cc6bad075f5
wyuan19asu <wyuan19@asu.edu>
2023-09-23 02:05:30 -0700

    added server side validation with Joi

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground");
 const methodOverride = require("method-override");
 const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
+const Joi = require("joi");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/yelp-camp")
@@ -58,6 +60,29 @@ app.get(
 app.post(
   "/campgrounds/",
   catchAsync(async (req, res, next) => {
+    // if (!req.body.campground)
+    //   throw new ExpressError("Invalid Campground Data", 400);
+
+    // validation using joi when using postman
+    // bc it still goes thru when everything else is empty
+    const campgroundSchema = Joi.object({
+      campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required(),
+      }).required(),
+    });
+
+    const { error } = campgroundSchema.validate(req.body);
+    console.log(error);
+
+    if (error) {
+      const msg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(msg, 400);
+    }
+
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground.id}`);
@@ -107,8 +132,14 @@ app.delete(
   })
 );
 
+app.use("*", (req, res, next) => {
+  next(new ExpressError("Page not found!", 404));
+});
+
 app.use((err, req, res, next) => {
-  res.send("Oh boy! Something went wrong");
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh No!, Something went wrong!";
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
